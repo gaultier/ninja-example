d928f4d0da9c17d163892ae9ba64f272b58a3227
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-03-04 16:23:55 +0100

    comment

--- rust_foo/src/lib.rs
+++ rust_foo/src/lib.rs
@@ -3,6 +3,5 @@ use std::ffi::c_char;
 #[no_mangle]
 pub extern "C" fn rust_foo_reverse(s: &mut c_char, len: usize) {
     let slice = unsafe { &mut *core::ptr::slice_from_raw_parts_mut(s, len) };
-    // foo!
     slice.reverse();
 }
