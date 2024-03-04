4031deb5ec36e1f940aeb25578524a5777d8542f
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-03-04 17:33:53 +0100

    tweak cbindgen

--- rust_foo/rust_foo.h
+++ rust_foo/rust_foo.h
@@ -1,11 +1,14 @@
-#include <cstdarg>
-#include <cstdint>
-#include <cstdlib>
-#include <ostream>
-#include <new>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif // __cplusplus
 
 void rust_foo_reverse(char *s, uintptr_t len);
 
+#ifdef __cplusplus
 } // extern "C"
+#endif // __cplusplus
