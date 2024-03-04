2b1bbf31d573c2c8317fca3958486a4ce1e79329
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-03-04 15:53:19 +0100

    fix

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,10 @@
 #include "lib.h"
 #include <utility>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 void reverse(char *s, uint64_t len) {
   for (int64_t i = len; i >= 0; i--) {
     std::swap(s[len - i], s[i]);
@@ -13,3 +17,7 @@ void upper_case_first(char *s, uint64_t len) {
   if ('a' <= s[0] && s[0] <= 'z')
     s[0] += 'a' - 'A';
 }
+
+#ifdef __cplusplus
+}
+#endif
--- lib.h
+++ lib.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
 void reverse(char *s, uint64_t len);
 void upper_case_first(char *s, uint64_t len);
+
+#ifdef __cplusplus
+}
+#endif
